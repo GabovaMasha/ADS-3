73a53db5a85da384c9f4183958a7e56e816ebc2c
GabovaMasha <94718616+GabovaMasha@users.noreply.github.com>
2022-03-13 22:09:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int a = 0, b = size - 1;
+int count = 0, f;
+while (a <= b) {
+int mid = a + (b - a) / 2;
+if (arr[mid] == value) {
+f = mid;
+count++;
+break;
+}
+if (arr[mid] > value)
+b = mid - 1;
+else
+a = mid + 1;
+}
+if (f >= 0) {
+for  (int i = 0; i < f; i++) {
+if (arr[i] == value)
+count++;
+}
+for (int j = size - 1; j > f; j--) {
+if (arr[j] == value)
+count++;
+}
+}
+return count;
 }
